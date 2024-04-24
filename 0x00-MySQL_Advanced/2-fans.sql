3543ad1ffa37b8bb93b76c80058e4b03f752fd09
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-24 21:21:48 +0100

    Fix column name in SQL script to rank country origins of bands by number of fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 -- ordered by the number of (non-unique) fans
 
 
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
