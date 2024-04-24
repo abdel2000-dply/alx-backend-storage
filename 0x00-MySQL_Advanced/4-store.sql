15817f17c631d9f23b18040256709ae522a7d58f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-24 23:20:49 +0100

    Refactor trigger in SQL script to decrease item quantity after adding a new order

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,8 +1,3 @@
 -- Creates a trigger that decreases the quantity of an item after adding a new order
-CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-  UPDATE items
-  SET quantity = quantity - NEW.number
-  WHERE name = NEW.item_name;
-END;
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
