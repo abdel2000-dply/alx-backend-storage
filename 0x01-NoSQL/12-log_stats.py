696263c0ae6147808306348af6ba4ba2416a961e
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-04-30 15:32:22 +0100

    Refactor log_stats script to use MongoClient with localhost and port 27017

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python3
-''' Log stats '''
+"""
+script that provides some stats about
+Nginx logs stored in MongoDB
+"""
+
 from pymongo import MongoClient
 
-
 def log_stats():
-    ''' Log stats '''
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    print(f"{total} logs")
+    """
+    Nginx logs stored in MongoDB
+    """
+    client = MongoClient('localhost', 27017)
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+
+    print(f"{total_logs} logs")
 
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = logs_collection.count_documents({"method": method})
-        print(f"    method {method}: {count}")
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
-    status_check = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    count_status = collection.count_documents({"method": "GET",
+                                                    "path": "/status"})
+    print(f"{count_status} status check")
 
 
 if __name__ == "__main__":
